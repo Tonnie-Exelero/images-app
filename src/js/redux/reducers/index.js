400c58244302533724bd148a58f71fbecc50e8c8
Tonnie Nyabuto <tonnieobuya@gmail.com>
2020-11-27 02:30:38 +0300

    Immutable arrays

--- src/js/redux/reducers/index.js
+++ src/js/redux/reducers/index.js
@@ -21,24 +21,24 @@ const initialState = {
 export default function rootReducer(state = initialState, action) {
   switch (action.type) {
     case "POSTS_LOADED": {
-      return {
+      return Object.assign({}, state, {
         ...state,
         posts: state.posts.concat(action.payload),
-      }
+      })
     }
 
     case "MEDIA_LOADED": {
-      return {
+      return Object.assign({}, state, {
         ...state,
         media: state.media.concat(action.payload),
-      }
+      })
     }
 
     case "USERS_LOADED": {
-      return {
+      return Object.assign({}, state, {
         ...state,
         users: state.users.concat(action.payload),
-      }
+      })
     }
 
     default:
